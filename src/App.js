50a69743466137fa2427e99397622aa86a821112
unknown <operator@stacja42-11.szkola.zst-radom.edu.pl>
2025-03-27 09:26:55 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+
+const categories = ["Ferrari", "Lamborghini", "Porsche", "BMW", "Mercedes", "Toyota","Lexus", "Audi", "McLaren"];
+const API_KEY = "49553999-d879b4443526b8658ddc629e9";
+const API_URL = "https://pixabay.com/api/";
+
+const App = () => {
+  const [category, setCategory] = useState("Ferrari");
+  const [images, setImages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch(
+          `${API_URL}?key=${API_KEY}&q=${category}+sports+car&image_type=photo&per_page=64`
+        );
+        const data = await response.json();
+        setImages(data.hits);
+      } catch (error) {
+        console.error("Błąd podczas pobierania danych:", error);
+      }
+    };
+
+    fetchImages();
+  }, [category]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Galeria Sportowych Samochodów</h1>
       </header>
+
+      <nav>
+        <ul>
+          {categories.map((cat) => (
+            <li key={cat}>
+              <button onClick={() => setCategory(cat)}>{cat}</button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <section className="gallery">
+        {images.map((img) => (
+          <figure key={img.id} onClick={() => setSelectedImage(img.largeImageURL)}>
+            <img src={img.previewURL} alt="" />
+          </figure>
+        ))}
+      </section>
+
+      {selectedImage && (
+        <div className="modal" onClick={() => setSelectedImage(null)}>
+          <img src={selectedImage} alt="Powiększone" />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
